e4cdfc175b9af610c73b7a6a5af673a806fcd69a
Wahyu Indra Cahyono <eindraz@gmail.com>
2022-03-10 20:18:59 +0700

    Read Data

--- app/src/main/java/com/wahyuindra/crudfirebase/MainActivity.kt
+++ app/src/main/java/com/wahyuindra/crudfirebase/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.wahyuindra.crudfirebase
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import data_mahasiswa
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -73,6 +72,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     }
                 })
             R.id.showdata -> {
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
             }
         }
     }
